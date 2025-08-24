5745dde51b72800091fe932ad7173eaaa8309023
Anil Rahman <anil_0000@hotmail.co.uk>
2025-08-24 17:42:11 +0100

    [web] custom video player design

--- web/src/app/gallery/gallery-all/gallery-all.ts
+++ web/src/app/gallery/gallery-all/gallery-all.ts
@@ -9,12 +9,18 @@ import { injectQuery } from '@tanstack/angular-query-experimental';
 import { Media } from '../../types/media';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { HlmIcon } from '@spartan-ng/helm/icon';
-import { lucidePlay } from '@ng-icons/lucide';
+import { lucideCirclePlay, lucideCirclePause } from '@ng-icons/lucide';
+import { VideoPlayer } from '@core/components/video-player/video-player';
 
 @Component({
   selector: 'app-gallery-all',
-  providers: [provideIcons({ lucidePlay })],
-  imports: [DatePipe, HlmSpinner, ErrorAlert, JsonPipe, NgIcon, HlmIcon],
+  providers: [
+    provideIcons({
+      lucideCirclePlay,
+      lucideCirclePause
+    }),
+  ],
+  imports: [DatePipe, HlmSpinner, ErrorAlert, JsonPipe, NgIcon, HlmIcon, VideoPlayer],
   template: `
     @if (files.isPending()) {
       <hlm-spinner />
@@ -41,18 +47,27 @@ import { lucidePlay } from '@ng-icons/lucide';
                 preload="metadata"
                 muted
                 playsInline
-                class="pointer-events-none absolute inset-0 h-full w-full object-cover"
+                class="absolute inset-0 h-full w-full object-cover"
+                (mouseenter)="onVideoHover(video)"
+                (mouseleave)="onVideoHoverOut(video)"
+                #video
               ></video>
 
-              <ng-icon
-                hlm
-                name="lucidePlay"
-                class="absolute top-2 right-2 h-6 w-6 drop-shadow-md"
-              ></ng-icon>
+              <div class="absolute top-2 right-1 dark:bg-black/30 rounded-full py-1 px-2 flex items-center gap-x-2">
+                <p class="font-semibold">0:09</p>
+                <ng-icon
+                  hlm
+                  [name]="videoHoverPlaying() ? 'lucideCirclePause' : 'lucideCirclePlay'"
+                  color="white"
+                  class="!block h-6 w-6 drop-shadow-md"
+                ></ng-icon>
+              </div>
             }
           </div>
         }
       </div>
+
+      <app-video-player source="http://localhost:3000/asset/d07de6ee-d0c9-407e-a889-651e1c408f98"></app-video-player>
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -62,8 +77,21 @@ export class GalleryAll {
 
   private readonly trpc = injectTrpc();
 
+  videoHoverPlaying = signal<boolean>(false);
+
   files = injectQuery(() => ({
     queryKey: [CacheKey.GalleryAll],
     queryFn: async () => this.trpc.files.getAllFiles.query(),
   }));
+
+  onVideoHover(video: HTMLVideoElement) {
+    video.play();
+    this.videoHoverPlaying.set(true);
+  }
+
+  onVideoHoverOut(video: HTMLVideoElement) {
+    video.pause();
+    video.currentTime = 0; // Reset to beginning
+    this.videoHoverPlaying.set(false);
+  }
 }
