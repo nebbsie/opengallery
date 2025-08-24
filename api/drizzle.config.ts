203bceab2b4006d27f577844a0aa61af044bdd3d
nebbsie <aaron@nebbs.com>
2025-08-24 20:52:44 +0100

    fix

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 export default {
   out: "./drizzle",
-  schema: ["./src/db/schema.ts", "./src/db/auth-schema.ts"],
+  schema: ["./src/db/schema.ts"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
